8fa05a0d9747e450bed7505ccac5f0897de1010c
ash-silver <passlion@naver.com>
2023-09-18 12:50:31 +0900

    Initial commit

---
+++ src/main/java/com/example/g_project/GProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.g_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/g_project/GProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
